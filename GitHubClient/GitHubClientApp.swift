fc972a25a193ac561a8accc658ecce56331df84e
Shiba <38932286+KaitakuShiba@users.noreply.github.com>
2021-10-13 08:19:08 +0900

    Initial Commit

---
+++ GitHubClient/GitHubClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubClientApp.swift
+//  GitHubClient
+//
+//  Created by Ikuho Shibata on 2021/10/13.
+//
+
+import SwiftUI
+
+@main
+struct GitHubClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
